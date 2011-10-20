eca48f4cda29307406af335a9ab958823b067868
Noah Seger <nosamanuel@gmail.com>
2011-10-20 15:34:19 -0500

    add purchase model

--- shoppinglist/models.py
+++ shoppinglist/models.py
@@ -12,3 +12,13 @@ class Ingredient(models.Model):
 
     def __unicode__(self):
         return u'%s - %s' % (self.name, LEVEL_CHOICES[self.level])
+
+
+class Purchase(models.Model):
+	ingredient = models.ForeignKey(Ingredient, related_name='purchases')
+	purchaser = models.ForeignKey('auth.User', related_name='purchases')
+    cost = models.DecimalField(blank=True, null=True)
+    date = models.DateField(auto_add_now=True)
+
+	def __unicode__(self):
+		return '%s purchased by %s' % (self.ingredient, self.purchaser)
